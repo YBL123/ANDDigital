5005dca6847e8e6db6a8ed823029f7056da15da3
Yarden Lawson <yardenlawson@gmail.com>
2021-02-18 15:18:11 +0000

    update

--- index.js
+++ index.js
@@ -41,18 +41,24 @@ function hasInt(str) {
 //permutation function
 function getArrayMutations(arr, perms = [], len = arr.length) {
   if (len === 1) perms.push(arr.slice(0))
-
+  console.log('length', len)
   for (let i = 0; i < len; i++) {
     getArrayMutations(arr, perms, len - 1)
-
+    console.log('len', len, 'modular:', len % 2)
+    // len % 2 - checking if true or false 
     len % 2 // parity dependent adjacent elements swap
-      ? [arr[0], arr[len - 1]] = [arr[len - 1], arr[0]]
-      : [arr[i], arr[len - 1]] = [arr[len - 1], arr[i]]
+      ? [arr[0], arr[len - 1]] = [arr[len - 1], arr[0]] // -> if true switch for new combo (so 326 becomes 236, 236 becomes 263, etc)
+      : [arr[i], arr[len - 1]] = [arr[len - 1], arr[i]] // -> if false takes current index value and switches it with the second last element of array
   }
 
+  console.log('perms', perms)
   return perms
 }
 
+const x = [3, 5, 6, 7]
+const y = [x[0], x[3]] = [x[3], x[0]]
+console.log('y', y)
+console.log('boop', Boolean(1))
 // some example inputs
 console.log(solution('326')); // expected ouput 632,623,362,326,263,236
 console.log(solution('A 3B2 C6D')); // expected ouput 632,623,362,326,263,236
